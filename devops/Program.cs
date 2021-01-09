071f36209681066a27f46088975d30df11427ce1
edsins <adityasingh_16738@aitpune.edu.in>
2021-01-09 12:46:02 +0530

    ddf

--- devops/Program.cs
+++ devops/Program.cs
@@ -11,7 +11,7 @@ namespace devops
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("code add now chnagess");
+            Console.WriteLine("code add now chnages2");
             Console.Read();
         }
     }
